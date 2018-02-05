aa55c4716948138e5341adda15c81b63a0e67389
Francis Calizo <francisacalizo@gmail.com>
2018-02-05 11:49:42 -0500

    update smoother car movement with drift-like aspects. no boundaries

--- src/script.js
+++ src/script.js
@@ -1,52 +1,70 @@
-// Selecting the canvas & two-dimensional context
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+		ctx = canvas.getContext('2d');
 
-//Player object
 var player = {
-  x: 225,
-  y: 225,
-  moveUp:    function(){ 
-    this.y -= 10 
+  x: 500, 
+  y: 250,
+  vx: 0, 
+  vy: 0,
+  ax: 0, 
+  ay: 0,
+  rotation: 0,
+  friction: .985,
+  
+	drawPlayer: function(){
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.rotate(this.rotation);
+		ctx.fillRect(0, 0, 40, 20);
+		ctx.restore();
   },
-  moveDown:  function() { this.y += 10 },
-  moveLeft:  function() { this.x -= 10 },
-  moveRight: function() { this.x += 10 },
-}
+  
+  updatePosition: function(){
+    //apply acceleration to velocity
+    this.vx += this.ax;
+    this.vy += this.ay;
+    this.updateFriction();
+    //Apply velocity to position
+    this.x += this.vx;
+    this.y += this.vy;
+    },
 
-//Update canvas per event
-function updateCanvas(){
-  clearCanvas();
-  drawPlayer(player); 
-  console.log(player.x);
-}
-
-//create player on canvas
-function drawPlayer(player){
-  ctx.fillRect(player.x, player.y, 50, 50);
-}
-
-//clear whole canvas
-function clearCanvas(){
-  ctx.clearRect(0, 0, 500, 500);
-}
-
-  document.onkeydown = function(e) {
-    //player movement
-    switch (e.keyCode) {
-      case 38: player.moveUp();    break;
-      case 40: player.moveDown();  break;
-      case 37: player.moveLeft();  break;
-      case 39: player.moveRight(); break;
-    }
-    //boundaries
-    switch (true){
-      case player.x < 0: player.x = 0; break;
-      case player.x > 450: player.x = 450; break;
-      case player.y < 0: player.y = 0; break;
-      case player.y > 450: player.y = 450; break;
-    }
-    updateCanvas();
+  updateFriction: function(){
+    //Apply friction to velocity to
+    this.vx *= this.friction;
+    this.vy *= this.friction;
   }
+};
+
+//user interactivity
+var keys = [];
+document.addEventListener('keydown', function(e){
+	keys[e.which] = true;
+});
+document.addEventListener('keyup', function(e){
+	keys[e.which] = false;
+});
+
+function updateCanvas(){
+	requestAnimationFrame(updateCanvas);
+	ctx.clearRect(0, 0, 1100, 620);
+	
+	//Player turning (Rotation)
+	if(keys[37]) player.rotation -= 0.04;
+	if(keys[39]) player.rotation += 0.04;
+	
+	//Accelerate Forward 
+	if(keys[38]){
+		player.ax = Math.cos(player.rotation) * 0.07;
+		player.ay = Math.sin(player.rotation) * 0.07;
+	} else{
+    //player acceleration should be zero if no keys are pressed
+		player.ax = player.ay = 0;
+	}
+	
+	//friction is applied inside the updatePosition function
+	player.updatePosition();
+	player.drawPlayer();
+};
 
 updateCanvas();
