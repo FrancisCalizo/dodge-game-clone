f4337e168b22161b02879f175d3196b24f21c5a3
Francis Calizo <francisacalizo@gmail.com>
2018-02-08 11:59:57 -0500

    add sprites and comment code

--- src/script.js
+++ src/script.js
@@ -1,21 +1,25 @@
 var canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-var gameOver = false;  
-var score = 0;
-var timeScore = setInterval(function(){score++}, 10);
-var backgroundMusic;
-backgroundMusic = new Audio("sound/backgroundMusic.mp3");
-backgroundMusic.volume= 1;
-var crash;
-crash = new Audio ("sound/crash.mp3");
-crash.volume=.2;
-var honk;
-honk = new Audio("sound/honk.mp3")
-honk.volume= 0.1;
 
-var honkHorn = setInterval(function(){
-  honk.play();
-},7000);
+var gameOver = false;  
+
+var score = 0;
+// Increment player score every 10 ms while !gameOver
+var timeScore = setInterval(function(){score++}, 10);
+
+// Declare sounds & music 
+var backgroundMusic = new Audio("sound/backgroundMusic.mp3");
+    backgroundMusic.volume= 1;
+var crash = new Audio ("sound/crash.mp3");
+    crash.volume=.2;
+var honk = new Audio("sound/honk.mp3")
+    honk.volume= 0.1;
+// Honk horn during game every 7 seconds
+var honkHorn = setInterval(function(){honk.play();},7000);
+// Declare player as batmobile
+var carSprite = new Image();
+carSprite.src = "Images/batmobile.png";
+
 
 // Player Object
 var player = {
@@ -30,13 +34,12 @@ var player = {
   
   // Draw player on canvas
 	drawPlayer: function(){
-		ctx.save();
-		ctx.translate(this.x, this.y);
+    ctx.save();
+    // Aid in turning player sprite
+    ctx.translate(this.x, this.y);
+    // Aid in turning player sprite
     ctx.rotate(this.rotation);
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    ctx.fillRect(0, 0, 40, 20);    
-    ctx.strokeStyle = "rgb(255, 0, 0)";
-    ctx.strokeRect(0, 0, 41 , 21);
+    ctx.drawImage(carSprite, 0, 0, 60, 30);
 		ctx.restore();
   },
 
@@ -58,13 +61,13 @@ var player = {
     this.vy *= this.friction;
   },
 
-  // Boundary check (faulty)
+  // Boundary check on player (faulty)
   boundaries: function(){
     switch(true){
-      case this.x < 20: this.x = 20; break; 
-      case this.x > 1080: this.x = 1080; break;
-      case this.y < 10: this.y = 10; break;
-      case this.y > 560: this.y = 560; break;
+      case this.x < 40: this.x = 40; break; 
+      case this.x > 1060: this.x = 1060; break;
+      case this.y < 40: this.y = 40; break;
+      case this.y > 550: this.y = 550; break;
     }
   }
 };
@@ -80,18 +83,19 @@ document.addEventListener('keyup', function(e){
 
 // Driver Object
 var drivers = {
-  spawnTopY: 0,
-  spawnBottomY: 580,
-  spawnRate: 350,
-  lastSpawn: -1,
-  driversArr: [],
+  spawnTopY: 0,       // Spawnpoint for cars moving down
+  spawnBottomY: 580,  // Spawnpoint for cars moving up
+  spawnRate: 350,     // Rate at which new cars spawn
+  lastSpawn: -1,      // Used in formula to determine if enough time passed to spawn new car
+  driversArr: [],     // Array that will store all drivers spawned
 
+  // Function to spawn random drivers
   spawnDriver: function(){
-    var speed;
-    var length;
-    var spawnPoint;
+    // Define characteristics of cars 
+    var speed, h, w, c, spawnPoint;
+    // Spawned driver type picked at random
     var rand = Math.random() * 2;
-
+    // 5 "types" of drivers that spawn
     if (rand < 0.4) {
       speed = 0.5;
       h = 60;
@@ -118,14 +122,16 @@ var drivers = {
       w = 50;
       c = "rgb(255, 255, 255)";
     }
-
+    // Determine if car spawns from top or bottom
     if(rand < 1.01){
+      // Spawn from bottom & reverse direction
       speed *= -1;
       spawnPoint = this.spawnBottomY;
     } else{
+      // Spawn from top and use default direction
       spawnPoint = this.spawnTopY;
     }
-
+    // Spawn Driver object to hold type features
     var driver = {
       height: h,
       width: w,
@@ -134,28 +140,29 @@ var drivers = {
       // Set x to start on random x position within canvas width
       x: Math.random() * (canvas.width - 30),
       // Set y to start on the line where objects are spawned
-      y: spawnPoint,
+      y: spawnPoint
     }
+    // Push to an Array to store drivers
     this.driversArr.push(driver);
   }
 }
 
+// Check player colliision with other drivers
 function checkCollision(){
-  crashPlay = 0;
-  for(var i = 0; i < drivers.driversArr.length; i ++){
+    for(var i = 0; i < drivers.driversArr.length; i ++){
     if (player.x < drivers.driversArr[i].x + drivers.driversArr[i].width &&
       player.x + 40 > drivers.driversArr[i].x &&
       player.y < drivers.driversArr[i].y + drivers.driversArr[i].height &&
       20 + player.y > drivers.driversArr[i].y) {
-      // collision detected
+      // collision detected, game is over
       gameOver = true;
-      clearInterval(timeScore);
-      clearInterval(honkHorn);
-      crash.play();
+      clearInterval(timeScore); // Stop player score from incrementing 
+      clearInterval(honkHorn);  // Stop Miami from honking at your life 
+      crash.play();             // Play crash sound when collision occurs
     }
   }
 }
-
+//Display final score & try again when collision occurs
 function gameOverText(){
   ctx.font = '72px sans-serif';
   ctx.textAlign="center"; 
@@ -165,17 +172,19 @@ function gameOverText(){
   ctx.fillText("Press the spacebar to retry!", canvas.width/2, canvas.height/1.4);  
 }
 
+// Main function that updates canvas
 function updateCanvas(){
   backgroundMusic.play();
   requestAnimationFrame(updateCanvas);
   ctx.clearRect(0, 0, 1100, 620);
+  // Score displayed on canvas & increments
   ctx.font = '180px sans-serif';
   ctx.textAlign="center"; 
   ctx.fillStyle = "rgb(50, 50, 50)";
   ctx.fillText(score, canvas.width/2, canvas.height/1.65);  
   ctx.strokeStyle = "rgb(255, 255, 255)";
   ctx.strokeText(score, canvas.width/2, canvas.height/1.65);
-  
+  // Keys array will hold user input * rotate player sprite
   switch(true){
     //Player turning (Rotation)
     case keys[37]: player.rotation -= 0.04; break;
@@ -183,21 +192,24 @@ function updateCanvas(){
   };
 
   switch(true){
-    //Accelerate Forward 
+    //Accelerate Forward with regard to player rotation
     case keys[38]: player.ax = Math.cos(player.rotation) * 0.07, 
                    player.ay = Math.sin(player.rotation) * 0.07; 
     break;
     //player acceleration should be zero if no keys are pressed                   
     default: player.ax = player.ay = 0;
   }
-
+  // Used for elasped time condition
   var time = Date.now();
-  // see if its time to spawn a new object
+  // See if its time to spawn a new object
   if (time > (drivers.lastSpawn + drivers.spawnRate)) {
+      // Set last spawn to current time
       drivers.lastSpawn = time;
+      // Spawn driver when condition is true
       drivers.spawnDriver();
   }
 
+  // assign characteristics to drivers in driversArr
   for (var i = 0; i < drivers.driversArr.length; i++) {
     var carSpawn = drivers.driversArr[i];
     carSpawn.y += carSpawn.spawnRateOfDescent;
@@ -206,21 +218,24 @@ function updateCanvas(){
     ctx.strokeStyle = "rgb(0, 0, 0)";
     ctx.strokeRect(carSpawn.x, carSpawn.y, carSpawn.width , carSpawn.height);
     }
-  
+  // Update player position 
   player.updatePosition();
-
+  // Only draw player when !gameOver
   if(!gameOver){
     player.drawPlayer();
   } else{
+    // When gameOver is true:
     gameOverText();
     backgroundMusic.pause()
     document.addEventListener('keydown', function(e){
       if(e.which === 32){
+        // Reload page with spacebar press
         window.location.reload(false); 
       }
     });
   }
+  // Check colliion on each frame
   checkCollision();
 };
- 
+// Begin game on window start
 updateCanvas();
